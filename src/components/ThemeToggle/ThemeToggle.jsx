077d15987bd2011473af231077b20e9ae542be4d
acol <acol248@gmail.com>
2022-07-31 18:58:40 +0100

    🚧 Introduced base theme toggle component

---
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect } from "react";
+
+import { ThemeContext } from "../../hooks/useTheme";
+
+// styles
+import styles from "./ThemeToggle.module.scss";
+import Icon from "./ThemeToggle.icons";
+
+export default function ThemeToggle({ deviceThemeMatch, ...props }) {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const handleThemeToggle = () => {
+    setTheme(theme === "night" ? "day" : "night");
+  };
+
+  useEffect(() => {
+    console.log(deviceThemeMatch);
+  }, [deviceThemeMatch]);
+
+  return (
+    <div className={styles["theme-toggle"]}>
+      <button onClick={handleThemeToggle}>
+        {theme === "day" ? <Icon type="day" /> : <Icon type="night" />}
+      </button>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const deviceThemeMatch = window.matchMedia("(prefers-color-scheme: dark)")
+    ? "night"
+    : "day";
+
+  return { props: { deviceThemeMatch } };
+}
