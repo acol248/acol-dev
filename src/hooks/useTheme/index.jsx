20f9ab0e06cc1bc6a6ac45c13207c988a14bbf14
acol <acol248@gmail.com>
2022-08-01 00:40:46 +0100

    🔥 removed code from wrong branch

--- src/hooks/useTheme/index.jsx
+++ src/hooks/useTheme/index.jsx
@@ -1,23 +1,11 @@
 import { createContext, useState } from "react";
 
 export default function useTheme() {
-  const [theme, setTheme] = useState("day");
-
-  const setThemeDetails = async (theme) => {
-    setTheme(theme);
-
-    await fetch("/api/theme/set-theme", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ theme }),
-    });
-  };
+  const [theme, setTheme] = useState("night");
 
   return {
     theme,
-    setTheme: setThemeDetails,
+    setTheme,
   };
 }
 
