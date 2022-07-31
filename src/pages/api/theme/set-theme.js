6ae640de4c5ab142d0e52c6f0c55c4bd2ecce4dc
acol <acol248@gmail.com>
2022-08-01 00:22:21 +0100

    🔥 Removed

--- src/pages/api/theme/set-theme.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (!res) res.status(400).json({ status: 400, message: "Bad Request" });
-
-  cookies.set("theme", req.theme);
-
-  res.status(200).json({ status: 200, message: "OK" });
-}
