f661b8093553d2fae40de90df3a85063b793646b
acol <acol248@gmail.com>
2022-07-31 21:10:29 +0100

    🚧 Adjustments to useTheme

--- src/hooks/useTheme/index.jsx
+++ src/hooks/useTheme/index.jsx
@@ -1,11 +1,23 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 
 export default function useTheme() {
   const [theme, setTheme] = useState("day");
 
+  const setThemeDetails = async (theme) => {
+    setTheme(theme);
+
+    await fetch("/api/theme/set-theme", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ theme }),
+    });
+  };
+
   return {
     theme,
-    setTheme,
+    setTheme: setThemeDetails,
   };
 }
 
