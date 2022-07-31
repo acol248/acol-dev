d5b0a1c25907d69f09ac1d5c62875d8c1cb4ad73
acol <acol248@gmail.com>
2022-07-31 18:58:00 +0100

    ✨ Updated footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,5 +2,12 @@
 import styles from "./Footer.module.scss";
 
 export default function Footer() {
-  return <footer className={styles.footer}></footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles['footer__title-box']}>
+        <p className={styles['footer__site-title']}>acol.dev</p>
+        <p className={styles['footer__copyright']}>acol.dev &copy; {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
 }
