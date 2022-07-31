7e357ce5592553bd8198c6aa5c7b4665e5575d4c
acol <acol248@gmail.com>
2022-07-31 18:58:10 +0100

    ✨ Updated navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 
+// components
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
+
 // styles
 import styles from "./Navbar.module.scss";
 
@@ -8,11 +11,30 @@ export default function Navbar({ items, ...props }) {
   return (
     <nav className={styles.navbar}>
       <div className={styles["navbar__items"]}>
-        {items.map((item, index) => (
-          <Link key={index} href={item.href}>
-            {item.name}
-          </Link>
-        ))}
+        {items.map(({ name, href, type }, index) => {
+          let out;
+
+          if (type === "internal") {
+            out = (
+              <Link href={href} key={index}>
+                <a className={styles["navbar__item"]}>{name}</a>
+              </Link>
+            );
+          }
+
+          if (type === "external") {
+            out = (
+              <a href={href} key={index} className={styles["navbar__item"]}>
+                {name}
+              </a>
+            );
+          }
+
+          return out;
+        })}
+      </div>
+      <div className={styles["navbar__items"]}>
+        <ThemeToggle />
       </div>
     </nav>
   );
