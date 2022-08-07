f0956a8a96b10faa2fb327db2c47a636f3c7d004
acol <acol248@gmail.com>
2022-08-07 12:16:30 +0100

    ✨ Updated for mobile nav menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,117 @@
+import { useState } from "react";
 import Link from "next/link";
+import Router from "next/router";
 
 // components
 import Button from "../Button/Button";
+import Modal from "../Modal";
 
 // styles
 import styles from "./Navbar.module.scss";
 import Icon from "./Navbar.icons";
+import { useEffect } from "react";
 
 export default function Navbar({ items, theme, themeChange, ...props }) {
+  const [isMobile, setIsMobile] = useState(false);
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  /**
+   * Handles theme toggle
+   */
   const handleThemeToggle = () => {
     themeChange(theme === "dark" ? "light" : "dark");
   };
 
+  useEffect(() => {
+    Router.events.on("routeChangeComplete", setMobileNavOpen(false));
+
+    return () =>
+      Router.events.off("routeChangeComplete", setMobileNavOpen(false));
+  }, []);
+
+  // sets isMobile to true if window width is less than 768px
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      /**
+       * IsMobile resize event listener
+       */
+      const handleIsMobile = () => {
+        setIsMobile(window.innerWidth < 768);
+      };
+
+      setIsMobile(window.innerWidth < 768);
+
+      window.addEventListener("resize", handleIsMobile);
+
+      return () => window.removeEventListener("resize", handleIsMobile);
+    }
+  }, []);
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles["navbar__links"]}>
-        {items.map(({ name, href, type }, index) => {
-          let out;
+      <Modal
+        open={mobileNavOpen}
+        onClose={() => setMobileNavOpen(false)}
+        variant="nav-menu"
+      >
+        <div className={styles['nav-menu__container']}>
+          {items &&
+            items.map(({ name, href, type, ...props }, index) => {
+              let out;
 
-          if (type === "internal") {
-            out = (
-              <Link href={href} key={index}>
-                <a className={styles["navbar__item"]}>{name}</a>
-              </Link>
-            );
-          }
+              if (type === "internal")
+                out = (
+                  <Link href={href} key={index}>
+                    <a className={styles["nav-menu__item"]}>{name}</a>
+                  </Link>
+                );
 
-          if (type === "external") {
-            out = (
-              <a href={href} key={index} className={styles["navbar__item"]}>
-                {name}
-              </a>
-            );
-          }
+              if (type === "external")
+                out = (
+                  <a
+                    href={href}
+                    key={index}
+                    className={styles["nav-menu__item"]}
+                  >
+                    {name}
+                  </a>
+                );
 
-          return out;
-        })}
-      </div>
+              return out;
+            })}
+        </div>
+      </Modal>
+
+      {isMobile && isMobile ? (
+        <button
+          onClick={() => setMobileNavOpen(true)}
+          className={styles["navbar__menu-button"]}
+        >
+          <Icon type="menu" />
+        </button>
+      ) : (
+        <div className={styles["navbar__links"]}>
+          {items.map(({ name, href, type }, index) => {
+            let out;
+
+            if (type === "internal")
+              out = (
+                <Link href={href} key={index}>
+                  <a className={styles["navbar__item"]}>{name}</a>
+                </Link>
+              );
+
+            if (type === "external")
+              out = (
+                <a href={href} key={index} className={styles["navbar__item"]}>
+                  {name}
+                </a>
+              );
+
+            return out;
+          })}
+        </div>
+      )}
 
       <div className={styles["navbar__items"]}>
         <button
