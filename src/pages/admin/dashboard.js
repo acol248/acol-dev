166ea4ad8f4c2618a9e9534d57e961747ee78e0c
acol <acol248@gmail.com>
2022-08-13 16:17:43 +0100

    ✨ Added admin pages

---
+++ src/pages/admin/dashboard.js
@@ -0,0 +1,12 @@
+// styles
+import styles from "../styles/Dashboard.module.scss";
+
+export default function Dashboard() {
+  return (
+    <div className={styles['container']}>
+      <div className={styles["container__inner"]}>
+
+      </div>
+    </div>
+  );
+}
