2d89992740c8eca6efaff0e6f787f3fe5e5f4359
acol248 <acol248@gmail.com>
2022-11-05 16:23:55 +0000

    🚧 document page

--- src/pages/documents/[target].js
+++ src/pages/documents/[target].js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
 
 import styles from "../../styles/Documents.module.scss";
 
@@ -6,13 +7,53 @@ export default function Post() {
   const router = useRouter();
   const { target } = router.query;
 
-  if (target) console.log(target);
+  const pageData = useRef(null);
+
+  const [querySuccess, setQuerySuccess] = useState(false);
+
+  useEffect(() => {
+    if (!target) return;
+
+    (async () => {
+      const request = await fetch(`/api/documents`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          target: target,
+        },
+      });
+
+      const result = await request.json();
+      pageData.current = result.content;
+      setQuerySuccess(() => pageData.current?.found);
+
+      console.log(pageData.current);
+    })();
+  }, [target]);
 
   return (
     <div className={styles["container"]}>
       <div className={styles["container__inner"]}>
         <div className={styles["container__header"]}>
-          <h2 className={styles["container__title"]}>{target}</h2>
+          <h2 className={styles["container__title"]}>
+            {querySuccess ? pageData.current?.title : `'${target}' not found.`}
+          </h2>
+        </div>
+
+        <div className={styles["container__content"]}>
+          {pageData.current &&
+            pageData.current.sections.map(({ title, type, content }) => {
+              let out;
+
+              switch (type) {
+                case 'text':
+                  break;
+                default:
+                  break;
+              }
+
+              return out;
+            })}
         </div>
       </div>
     </div>
