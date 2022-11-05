47e3acd7ae2eb1d8f9a57ea3a2098d87f3b9803b
acol248 <acol248@gmail.com>
2022-11-05 16:23:35 +0000

    documents api

---
+++ src/pages/api/documents.js
@@ -0,0 +1,45 @@
+import dbConnect from "../../lib/dbconnect/dbconnect.js";
+
+// Schemas
+import Document from "../../database/Documents.js";
+
+const handlePost = async (req, res) => {
+  return res.status(200).json({ status: 200, body: "Success!" });
+};
+
+const handleGet = async (req, res) => {
+  if (!req.headers.target)
+    return res
+      .status(400)
+      .json({ status: 400, body: "'target' missing from headers." });
+
+  const target = req.headers.target;
+
+  await dbConnect();
+
+  const data = await Document.findOne({ name: target });
+
+  return res.status(200).json({
+    status: 200,
+    body: {
+      text: "Success!",
+      found: data !== null,
+      data: data,
+    },
+  });
+};
+
+export default async function handler(req, res) {
+  if (!req) return res.status(400).json({ status: 400, body: "Missing req!" });
+
+  switch (req.method) {
+    case "POST":
+      return handlePost(req, res);
+    case "GET":
+      return handleGet(req, res);
+    default:
+      return res
+        .status(500)
+        .json({ status: 500, body: "Error handling request method." });
+  }
+}
