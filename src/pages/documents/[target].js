f873e3405e87b21731b027dd16961f5719265e1e
acol <acol248@gmail.com>
2022-11-18 18:52:50 +0000

    🚧 documents file

--- src/pages/documents/[target].js
+++ src/pages/documents/[target].js
@@ -1,8 +1,9 @@
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
+import PageMedia from "../../components/PageMedia";
 
 // components
-import Text from "../../components/Text";
+import PageText from "../../components/PageText";
 
 // styles
 import styles from "../../styles/Documents.module.scss";
@@ -11,12 +12,11 @@ export default function Post() {
   const router = useRouter();
   const { target } = router.query;
 
-  const pageData = useRef(null);
-
+  const [pageData, setPageData] = useState(null);
   const [querySuccess, setQuerySuccess] = useState(false);
 
   useEffect(() => {
-    if (!target) return;
+    if (!target || pageData) return;
 
     (async () => {
       const request = await fetch(`/api/documents`, {
@@ -28,34 +28,45 @@ export default function Post() {
       });
 
       const result = await request.json();
-      pageData.current = result.content;
-      setQuerySuccess(() => pageData.current?.found);
 
-      console.log(result);
+      setPageData(result.body.data);
+      setQuerySuccess(result.body?.found);
     })();
-  }, [target]);
+  }, [pageData, target]);
 
   return (
     <div className={styles["container"]}>
       <div className={styles["container__inner"]}>
         <div className={styles["container__header"]}>
           <h2 className={styles["container__title"]}>
-            {querySuccess ? pageData.current?.title : `'${target}' not found.`}
+            {querySuccess ? pageData.title : `'${target}' not found.`}
           </h2>
         </div>
 
         <div className={styles["container__content"]}>
-          {pageData.current &&
-            pageData.current.sections.map(({ className, name, type, content }) => {
+          {pageData &&
+            pageData.sections?.map(({ className, name, type, content }) => {
               let out;
 
               switch (type) {
                 case "text":
                   out = (
-                    <Text
+                    <PageText
+                      key={name + type}
                       className={className}
                       title={content.title}
                       subtitle={content.subtitle}
+                      body={content.body}
+                    />
+                  );
+
+                  break;
+                case "video":
+                  out = (
+                    <PageMedia
+                      key={name + type}
+                      video={content.src}
+                      title={content.title}
                       body={content.body}
                     />
                   );
