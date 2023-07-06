c81e6734950d47c4523a41185241bdb0304a3079
Alex Collyer <acol248@gmail.com>
2023-07-06 15:38:16 +0100

    Added base for Projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,18 @@
+// components
+import Section from "@/components/Section";
+
+// styles
+import styles from "@/styles/Projects.module.scss";
+import { CSSProperties } from "react";
+
+export default function Projects() {
+  return (
+    <section className={styles["projects"]}>
+      <div className={styles["projects__content"]}>
+        <Section style={{ minHeight: "calc(100vh - 72px)" }}>
+          
+        </Section>
+      </div>
+    </section>
+  );
+}
