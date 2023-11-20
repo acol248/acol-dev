4cc0dc5f05b8f4f8770501356be1e7ebfa76770f
acol248 <acol248@gmail.com>
2023-11-20 00:26:58 +0000

    🚧 Testing

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,15 +4,15 @@ import Section from "@/components/Section";
 
 // styles
 import styles from "@/styles/Projects.module.scss";
-import { CSSProperties } from "react";
 
 export default function Projects() {
   return (
     <section className={styles["projects"]}>
       <div className={styles["projects__content"]}>
         <Section style={{ minHeight: "calc(100vh - 72px)" }}>
-          <DetailedButton>
+          <DetailedButton className={styles['projects__button']}>
             <h3>Title</h3>
+            <p>I am a description for the button.</p>
           </DetailedButton>
         </Section>
       </div>
