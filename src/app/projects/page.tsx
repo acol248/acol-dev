ddc0785ea795b291488892bc22a9a9e4e2fd498c
acol248 <alex.collyer@acol.dev>
2024-01-24 00:10:45 +0000

    Updated Projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,18 @@
-// components
-import DetailedButton from "@/components/DetailedButton";
-import Section from "@/components/Section";
+// parts
+import ProjectsPart from "@/parts/Projects";
 
 // styles
-import styles from "@/styles/Projects.module.scss";
+import styles from "@/styles/Projects/Projects.module.scss";
 
 export default function Projects() {
   return (
     <section className={styles["projects"]}>
+      <div className={styles["projects__header"]}>
+        <h2 className={styles["projects__title"]}>Projects</h2>
+      </div>
+
       <div className={styles["projects__content"]}>
-        <Section style={{ minHeight: "calc(100vh - 72px)" }}>
-          <DetailedButton className={styles['projects__button']}>
-            <h3>Title</h3>
-            <p>I am a description for the button.</p>
-          </DetailedButton>
-        </Section>
+        <ProjectsPart />
       </div>
     </section>
   );
