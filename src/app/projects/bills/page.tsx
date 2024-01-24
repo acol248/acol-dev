50b02b6c91ba344ae0531507be45245e94cb2656
acol248 <alex.collyer@acol.dev>
2024-01-24 00:11:30 +0000

    Added base Bills page

---
+++ src/app/projects/bills/page.tsx
@@ -0,0 +1,10 @@
+// styles
+import styles from "@/styles/Projects/Bills.module.scss";
+
+export default function Bills() {
+  return (
+    <section className={styles["bills"]}>
+
+    </section>
+  );
+}
