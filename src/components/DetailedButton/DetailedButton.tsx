7a389c5fff6b4aea2cd5d2912d27fd81247fa42c
acol248 <alex.collyer@acol.dev>
2024-06-06 21:08:46 +0100

    💄 Detailed Button

--- src/components/DetailedButton/DetailedButton.tsx
+++ src/components/DetailedButton/DetailedButton.tsx
@@ -11,16 +11,24 @@ interface IDetailedButton extends HTMLProps<HTMLButtonElement> {
   variant?: string;
   tags?: string[];
   ident?: ReactNode;
+  body: string;
 }
 
-export default function DetailedButton({ className, variant, ident, tags = [], children, onClick }: IDetailedButton) {
+export default function DetailedButton({
+  className,
+  variant,
+  ident,
+  tags = [],
+  body,
+  onClick,
+}: IDetailedButton) {
   const classList = useClassList({ defaultClass: "detailed-button", className, variant, maps, string: true }) as string;
 
   return (
     <button className={classList} onClick={onClick} type="button">
       {ident && <div className={mc("detailed-button__ident")}>{ident}</div>}
       <div className={mc("detailed-button__content")}>
-        {children}
+        <p className={mc('detailed-button__body')}>{body}</p>
 
         <div className={mc("detailed-button__tags")}>
           {Boolean(tags.length > 0) &&
